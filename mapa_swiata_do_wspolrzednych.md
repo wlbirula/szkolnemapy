98624095fe75ca617f5ce071460b2a9c2b2618bd
wlbirula <w.birula@gmail.com>
2022-07-09 08:30:06 +0200

    Test SVG

--- mapa_swiata_do_wspolrzednych.md
+++ mapa_swiata_do_wspolrzednych.md
@@ -2,8 +2,5 @@ Mapa świata do ćwiczenia współrzędnych geograficznych
 
 ![mapamapa_swiata_do_wspolrzednych](images/mapa_swiata_do_wspolrzednych.svg)
 
-Inne wersje tej mapy do pobrania:
-* wersja do druku w formacie [PDF](images/mapa_swiata_do_wspolrzednych.pdf)
-
 Źródło:
 * [skrypt](scripts/mapa_swiata_do_wspolrzednych.sh) do programu Generic Mapping Tools
--- scripts/mapa_swiata_do_wspolrzednych.sh
+++ scripts/mapa_swiata_do_wspolrzednych.sh
@@ -1,6 +1,6 @@
 gmt gmtset FORMAT_GEO_MAP dddA 
 gmt gmtset FONT_ANNOT_PRIMARY 8
 gmt pscoast -R-180/180/-75/75 -JQ0/18c -B20g10WsNe -Wblack -Dl -A5000 -Swhite -G#ccccff  -P > ../images/mapa_swiata_do_wspolrzednych.ps
-gmt psconvert ../images/mapa_swiata_do_wspolrzednych.ps -Tj -Am1c -E600
-gmt psconvert ../images/mapa_swiata_do_wspolrzednych.ps -Ts -Am1c -E600
+gmt psconvert ../images/mapa_swiata_do_wspolrzednych.ps -Tf -A1c -E600
+pdf2svg ../images/mapa_swiata_do_wspolrzednych.pdf ../images/mapa_swiata_do_wspolrzednych.svg
 
